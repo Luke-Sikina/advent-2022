b95781198da69c42987d03fde9e90e9fcb243e8f
Luke Sikina <lucas@ds.dfci.harvard.edu>
2022-12-06 11:09:33 -0500

    Day 6 P 2

--- day-6/src/main/java/Solution.java
+++ day-6/src/main/java/Solution.java
@@ -3,29 +3,44 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
+        System.out.println("Day 6 Part 1");
         s.parseInput()
             .map(s::part1)
             .ifPresentOrElse(
                 u -> System.out.println("The sequence is: " + u),
                 () -> System.out.println("Bad input")
             );
+
+        System.out.println("Day 6 Part 2");
+        s.parseInput()
+            .map(s::part2)
+            .ifPresentOrElse(
+                u -> System.out.println("The sequence is: " + u),
+                () -> System.out.println("Bad input")
+            );
+    }
+    private int part2(String in) {
+        return getFirstUniqueSeq(in, 14).orElse(-1);
     }
 
-    private int part1(String in) {
-        for (int i = 4; i < in.length(); i++) {
-            String current = in.substring(i - 4, i);
-            if (current.chars().distinct().count() == 4) {
-                return i;
+    private Optional<Integer> getFirstUniqueSeq(String in, int len) {
+        for (int i = len; i < in.length(); i++) {
+            String current = in.substring(i - len, i);
+            if (current.chars().distinct().count() == len) {
+                return Optional.of(i);
             }
         }
+        return Optional.empty();
+    }
 
-        return -1;
+
+    private int part1(String in) {
+        return getFirstUniqueSeq(in, 4).orElse(-1);
     }
 
     private Optional<String> parseInput() {
